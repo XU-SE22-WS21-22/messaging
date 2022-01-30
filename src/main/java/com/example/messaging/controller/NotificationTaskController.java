d78603312c415aff77991857ccda40cf50fc16c0
fipso <philippliermann@gmx.de>
2022-01-30 19:19:00 +0100

    added example notification task logic

--- src/main/java/com/example/messaging/controller/NotificationTaskController.java
+++ src/main/java/com/example/messaging/controller/NotificationTaskController.java
@@ -1,8 +1,10 @@
 package com.example.messaging.controller;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
-import com.example.messaging.model.NotifiTask;
+import com.example.messaging.model.NotificationTask;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NotificationTaskController {
 
-    @GetMapping("/notificationtask")
-    public ArrayList<NotifiTask> notification(@RequestParam(value = "userId") int userId) {
-        NotifiTask task = new NotifiTask(userId, "test");
-        ArrayList<NotifiTask> list = new ArrayList<NotifiTask>();
+    @GetMapping("/api/NotificationTasks")
+    public ArrayList<NotificationTask> notification(@RequestParam(value = "userId") int userId) {
+
+       /*
+
+       TODO explain why this is like it is
+
+        */
+
+
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 2);
+        Date date = c.getTime();
+
+        NotificationTask task = new NotificationTask(0, "Example Task",
+                "lorem lorem lorem", "/tasks/0", date);
+        NotificationTask task2 = new NotificationTask(1, "Example Task 2",
+                "lorem lorem lorem", "/tasks/1", date);
+        ArrayList<NotificationTask> list = new ArrayList<>();
         list.add(task);
-        list.add(task);
+        list.add(task2);
         return list;
     }
 }
--- src/main/java/com/example/messaging/model/NotificationTask.java
+++ src/main/java/com/example/messaging/model/NotificationTask.java
@@ -1,7 +1,61 @@
 package com.example.messaging.model;
 
-import java.util.List;
+import java.util.Date;
 
 public class NotificationTask {
-    List<UserMissedForms> usersMissedForms;
+    private long id;
+    private String title;
+    private String description;
+    private String url;
+    private Date deadline;
+
+    public NotificationTask(long id, String title, String description, String url, Date deadline) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.deadline = deadline;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+
 }
